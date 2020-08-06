2c06a8f7f9ef6d80a8c0738ab1bea8a0c5a62c60
rinkei <rinkei.krt6006@gmail.com>
2020-08-06 17:12:59 +0900

    :tada: Inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('dotenv').config();
+const ENV = process.env;
+const ngrok = require('ngrok');
+const isOnline = require('is-online');
+const delay = require('delay');
+const { IncomingWebhook } = require('@slack/webhook');
+const webhook = new IncomingWebhook(ENV.SLACK_WEBHOOK_URL);
+
+(async () => {
+  while (!await isOnline()) {
+    await delay(3000);
+  }
+  await ngrok.authtoken(ENV.NGROK_TOKEN);
+  const ngrokUrl = await ngrok.connect({proto: 'tcp', addr: ENV.SSH_PORT});
+  await webhook.send({
+    text: `server restarted: ${ngrokUrl}`
+  });
+})();
