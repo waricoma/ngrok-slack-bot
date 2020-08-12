adb882bbdcad1af7a1986ea11a7c3ca85821d48c
waricoma <waricoma@devne.co>
2020-08-12 14:26:45 +0900

    :wrench: update: eslint-config

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const { IncomingWebhook } = require('@slack/webhook');
 const webhook = new IncomingWebhook(ENV.SLACK_WEBHOOK_URL);
 
 (async () => {
-  while (!await isOnline()) {
+  while (!(await isOnline())) {
     await delay(3000);
   }
   await ngrok.authtoken(ENV.NGROK_TOKEN);
-  const ngrokUrl = await ngrok.connect({proto: 'tcp', addr: ENV.SSH_PORT});
+  const ngrokUrl = await ngrok.connect({ proto: 'tcp', addr: ENV.SSH_PORT });
   await webhook.send({
-    text: `server restarted: ${ngrokUrl}`
+    text: `server restarted: ${ngrokUrl}`,
   });
 })();
