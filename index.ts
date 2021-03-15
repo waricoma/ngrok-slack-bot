57f2ec1073b300031aae8a130d058f5d9c4ca077
rinkei <6terabyte.onlyreceive@gmail.com>
2021-03-15 23:28:21 +0900

    :sparkles: for new server

--- index.ts
+++ index.ts
@@ -7,56 +7,38 @@ const ENV = process.env;
 import isOnline from 'is-online';
 import delay from 'delay';
 import { IncomingWebhook } from '@slack/webhook';
-import ip from 'ip';
-import fs from 'fs';
-import path from 'path';
-import { spawn } from 'child_process';
-import yaml from 'js-yaml';
+//import ip from 'ip';
+import { exec, execSync } from 'child_process';
 
 const webhook = new IncomingWebhook(ENV.SLACK_WEBHOOK_URL);
 
-const binPath = path.resolve(__dirname, './node_modules/ngrok/bin/ngrok');
-
-const configPath = path.resolve(__dirname, './config.yml');
-const configStr = fs.readFileSync(configPath, 'utf8');
-
-const tunnels = yaml.safeLoad(configStr)['tunnels'];
-const tunnelsNum = Object.keys(tunnels).length;
-
-const ops = ['start', '--all', '--log=stdout', `--config=${configPath}`];
-
-let tunnelsCount = 0;
-let tunnelsReport = `🚨 server restarted!\nip address: ${ip.address()}`;
-
-const ngrokHandler = (data: string) => {
-  const msg = data.toString().trim();
-
-  if (msg.indexOf('lvl=info msg="started tunnel" obj=tunnels') === -1) {
-    return;
-  }
-
-  if (msg.indexOf('url=http://') >= 0) {
-    return;
-  }
-
-  console.log(msg);
-
-  tunnelsReport += `\n${msg.split('name=')[1]}`;
-  ++tunnelsCount;
-
-  if (tunnelsCount !== tunnelsNum) {
-    return;
-  }
-
-  webhook.send({
-    text: tunnelsReport,
-  });
+const getGlobalIP = () => {
+  return String(execSync('curl -s https://checkip.amazonaws.com')).trim();
 };
 
 (async () => {
   while (!(await isOnline())) {
     await delay(3000);
   }
-  const ngrok = spawn(binPath, ops);
-  ngrok.stdout.on('data', ngrokHandler);
+  if ((ENV.X11VNC || '').toLowerCase() === 'true') {
+    exec('x11vnc -forever -display :0 -rfbauth ~/.x11vnc/passwd -localhost');
+  }
+  let restartMsg = 'server restarted:\n';
+  let address = getGlobalIP(); //ip.address();
+  restartMsg += `ip: ${address}\n`;
+  if (ENV.NGROK_TOKEN) {
+    await ngrok.authtoken(ENV.NGROK_TOKEN);
+    restartMsg += `ngrok: ${await ngrok.connect({
+      proto: 'tcp',
+      addr: ENV.SSH_PORT,
+    })}\n`;
+  }
+  await webhook.send({ text: restartMsg });
+
+  setInterval(async () => {
+    if (getGlobalIP() != address) {
+      address = getGlobalIP();
+      await webhook.send({ text: `ip changed: ${address}` });
+    }
+  }, 5000);
 })();
